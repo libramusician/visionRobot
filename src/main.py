74713408b55995c3dc042feeb90b21ed6b125574
libramusician <dongguanqun@126.com>
2022-07-18 04:21:04 +0500

    petry, ip camera

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 import numpy as np
+import requests
 
+url = "http://192.168.1.3:8080/shot.jpg"
 face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
 buffer = np.zeros((10,4), int)
 
@@ -13,17 +15,19 @@ def object_detect(img, cascade):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    cap = cv.VideoCapture(0)
-    if not cap.isOpened():
-        print("Cannot open camera")
-        exit()
+    # cap = cv.VideoCapture("https://192.168.1.3:8080")
+    # if not cap.isOpened():
+    #     print("Cannot open camera")
+    #     exit()
     while True:
         # Capture frame-by-frame
-        ret, frame = cap.read()
+        # ret, frame = cap.read()
+
         # if frame is read correctly ret is True
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
+        # if not ret:
+        #     print("Can't receive frame (stream end?). Exiting ...")
+        #     break
+        frame = requests.get(url)
         object_detect(frame, face_cascade)
 
         cv.imshow("frame",frame)
@@ -31,5 +35,5 @@ if __name__ == '__main__':
         if cv.waitKey(16) == ord('q'):
             break
     # When everything done, release the capture
-    cap.release()
+#    cap.release()
     cv.destroyAllWindows()
