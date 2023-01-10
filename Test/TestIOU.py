49e9040872ba842941425b41261af417651353e9
jiatonghan1118 <jiatonghan@cmail.carleton.ca>
2023-01-10 17:48:28 -0500

    Test IOU and Code DOC

---
+++ Test/TestIOU.py
@@ -0,0 +1,10 @@
+from src.boundingBox import *
+import unittest
+
+
+class TestIOU(unittest.TestCase):
+    def testIOU(self):
+        box1 = BoundingBox((1, 2, 30, 40))
+        box2 = BoundingBox((3, 4, 30, 40))
+
+        self.assertTrue(iou(box1, box2) - 0.7964 < 0.001)
